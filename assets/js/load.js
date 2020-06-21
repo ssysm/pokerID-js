cc2b53c7f2c9b3e3856ccdac28021eb95bd658c1
ssysm <contact@theeditorstudio.com>
2020-06-21 14:30:46 -0400

    refact dir

--- assets/js/load.js
+++ assets/js/load.js
@@ -2,7 +2,7 @@ function loadImage(){
     for(var i = 0; i < suit.length;i++){
         for(var j = 0; j < rankChar.length;j++){
             let img = new Image(150,320);
-            img.src = `./img/${rankChar[j]}${suit[i].slice(0,1)}.jpg`;
+            img.src = `./assets/img/${rankChar[j]}${suit[i].slice(0,1)}.jpg`;
             cardImg.push(img.outerHTML);
         }
     }
